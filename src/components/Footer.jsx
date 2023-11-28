8196eeed7171dbe9add408343c845f88619825f3
Your Name <you@example.com>
2023-11-28 20:48:17 +0100

    from yassin to fadma

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+function Footer(){
+
+    return (
+        <div className="footer">
+        <h1>Content Of Footer</h1>
+        </div>
+    )
+}
+
+
+
+export default Footer
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+
+
+function NavBar(){
+
+    return (
+    <div className="NavBar">
+        <h1>Content Of NavBar</h1>
+    </div>
+    )
+}
+
+
+
+export default NavBar
