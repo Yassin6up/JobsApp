e7d318e3570575b3f94fe8062ad9dfbf2042c942
Your Name <you@example.com>
2023-12-03 18:17:11 +0100

    03/12 💫

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import react from "react"
-
-/**
-  * 1. Create a variable which had your name
-  * * * and put it into a jsx element.
-  * 2. Create an external CSS file and link to the component.
-  * 3. Change color of the element to red
-  * * * and text size to 35px.
-  */
-
+import CardCompnent from "./components/Card"
+import "./style.css"
 function App() {
+
+
   return (
-    <div>
-      content
-    </div>
+    <div className="container">
+  <CardCompnent title = "iphone 11"  desc= "iphone for sell 150$" button="buy now" photo="https://th.bing.com/th?q=Pixel+6A&w=120&h=120&c=1&rs=1&qlt=90&cb=1&pid=InlineBlock&mkt=en-XA&cc=MA&setlang=en&adlt=strict&t=1&mw=247"/>
+  <CardCompnent title = "iphone 12"  desc= "iphone for sell 550$" button="buy now" photo="https://www.bing.com/th/id/OIP.5ckFxySNflG-JDBYH0D20AHaJ4?w=133&h=180&c=7&r=0&o=5&pid=1.7"/>
+
+  </div>
   );
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+function CardCompnent(props) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={props.photo}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {props.desc}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          {props.button}
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+
+export default CardCompnent
